6a52e86d6c7bf09860fa16742a096fc1e1a37141
Derek Nash <pluraldj@gmail.com>
2011-08-31 00:03:30 -0500

    new CreateAccount project

---
+++ CreateAccount/CreateAccount/SteamCraftConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CreateAccount
+{
+    class SteamCraftConfig
+    {
+        
+        public static string SteamCraftString()
+        {
+
+            return ConfigurationManager.ConnectionStrings["SteamCraftConnectionString"].ConnectionString;
+        
+        }
+
+        public void SteamCraftConn()
+        {
+            SqlConnection scConn = new SqlConnection(SteamCraftString());
+
+        }
+
+    }
+}
