d7ea803de7efdddc053dfccb3716b69c914eb320
Derek Nash <pluraldj@gmail.com>
2011-09-03 22:01:43 -0500

    new Login app - initial commit

---
+++ LoginToAccount/LoginToAccount/SteamCraft.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace LoginToAccount
+{
+    class SteamCraft
+    {
+        public void connectSteamCraft()
+        {
+            SqlConnection steamConn = new SqlConnection(ConfigurationManager.ConnectionStrings["SteamCraftConnectionString"].ConnectionString);
+            steamConn.Open();
+        }
+
+        public string getSalt(string emailaddress)
+        {
+            SqlCommand getsalt = new SqlCommand("spLogin", this.connectSteamCraft);
+            getsalt.CommandType = CommandType.StoredProcedure;
+            getsalt.Parameters.AddWithValue("@EmailAddress", emailaddress);
+            string passwordsalt = (string)getsalt.ExecuteScalar();
+            return passwordsalt;
+        }
+    }
+}
